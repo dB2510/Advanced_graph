c7c0870bcf0caa46c4e4c0f6703b769f1795730f
dB2510 <dhruvbodani2510@gmail.com>
2019-06-07 14:57:13 +0530

    finding connected components

---
+++ get_connected_components.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+#include<vector>
+#include<unordered_set>
+using namespace std;
+
+void dfs(vector<int>*edges, int start, unordered_set<int>*component, bool *visited)
+{
+    visited[start]=true;
+    component->insert(start);
+    for(int i=0;i<edges[start].size();i++)
+    {
+        int next=edges[start][i];
+        if(!visited[next])
+        {
+            dfs(edges,next,component,visited);
+        }
+    }
+}
+
+
+unordered_set<unordered_set<int>*>* getcompo(int n, vector<int>*edges)
+{
+    bool visited[n+1];
+    for(int i=0;i<=n;i++)
+    visited[i]=false;
+    unordered_set<unordered_set<int>*>* output = new unordered_set<unordered_set<int>*>();
+    for(int i=1;i<=n;i++)
+    {
+        if(!visited[i])
+        {
+            unordered_set<int>*component = new unordered_set<int>();
+            dfs(edges,i,component,visited);
+            output->insert(component);
+        }
+    }
+    return output;
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,m;
+        cin>>n>>m;
+        vector<int>edges[n+1];
+        for(int i=0;i<m;i++)
+        {
+            int a,b;
+            cin>>a>>b;
+            edges[a].push_back(b);
+            edges[b].push_back(a);
+        }
+        unordered_set<unordered_set<int>*>*components = getcompo(n, edges);
+        unordered_set<unordered_set<int>*>::iterator it=components->begin();
+        while (it != components->end()) {
+		    unordered_set<int>* component = *it;
+		    unordered_set<int>::iterator it2 = component->begin();
+		    while (it2 != component->end()) {
+		            cout<<*it2<<" ";
+			    it2++;
+		    }
+		    cout<<endl;
+		    delete component;
+		    it++;
+	    }
+	delete components;
+    }
+	return 0;
+}
+
