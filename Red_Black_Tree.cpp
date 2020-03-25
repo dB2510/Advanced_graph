2d3fe62b4aa89b501700de141b2a447952ef2070
dB2510 <dhruvbodani2510@gmail.com>
2020-03-26 03:30:13 +0530

    RB_INSERT

--- Red_Black_Tree.cpp
+++ Red_Black_Tree.cpp
@@ -30,7 +30,57 @@ void LEFT_ROTATE(struct Node* node)
     node -> parent = y;
 }
 
-void RIGHT_ROTATE(struct N)
+void RIGHT_ROTATE(struct Node* node)
+{
+    struct Node* y;
+    y = node -> left;
+    node -> left = y -> right;
+    if(y -> right != nil)
+    y -> right -> parent = node;
+    y -> parent = node -> parent;
+    if(node -> parent != nil)
+    root = y;
+    else if(node == node -> parent -> left)
+    node -> parent -> left = y;
+    else
+    node -> parent -> right = y;
+
+    y -> right = node;
+    node -> parent = y;
+}
+
+void INSERT_FIXUP(struct Node* node)
+{
+    
+}
+
+
+void INSERT(struct Node* node)
+{
+    struct Node* y;
+    struct Node* x;
+    y = nil;
+    x = root;
+    while(x != nil)
+    {
+        y = x;
+        if(node -> data < x -> data)
+        x = x -> left;
+        else
+        x = x -> right;
+    }
+    node -> parent = y;
+    if(y == nil)
+    root = node;
+    else if(node -> data < y -> data)
+    y -> left = node;
+    else
+    y -> right = node;
+    node -> left = nil;
+    node -> right = nil;
+    node -> color = "red";
+    INSERT_FIXUP(node);
+}
 
 int main()
 {
