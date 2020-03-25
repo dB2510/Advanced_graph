79dbd0d865b789026d94262c2dbceede44efc68a
dB2510 <dhruvbodani2510@gmail.com>
2020-03-26 05:13:24 +0530

    Debugging_insertion

--- Red_Black_Tree.cpp
+++ Red_Black_Tree.cpp
@@ -11,6 +11,17 @@ struct Node{
 struct Node* nil;
 struct Node* root;
 
+struct Node* newNode(int item)
+{
+    struct Node* temp = (struct Node *) malloc(sizeof(struct Node));
+    temp -> data = item;
+    temp -> left = nil;
+    temp -> right = nil;
+    temp -> parent = nil;
+    temp -> color = "red";
+    return temp;
+}
+
 void LEFT_ROTATE(struct Node* node)
 {
     struct Node* y;
@@ -19,7 +30,7 @@ void LEFT_ROTATE(struct Node* node)
     if(y -> left != nil)
     y -> left -> parent = node;
     y -> parent = node -> parent;
-    if(node -> parent != nil)
+    if(node -> parent == nil)
     root = y;
     else if(node == node -> parent -> left)
     node -> parent -> left = y;
@@ -34,12 +45,17 @@ void RIGHT_ROTATE(struct Node* node)
 {
     struct Node* y;
     y = node -> left;
+    // std::cout << y -> data;
     node -> left = y -> right;
     if(y -> right != nil)
     y -> right -> parent = node;
     y -> parent = node -> parent;
-    if(node -> parent != nil)
-    root = y;
+    // std::cout << node -> data << std::endl;
+    if(node -> parent == nil)
+    {
+        // std::cout << "aagyabhaiidhar" << std::endl;
+        root = y;
+    }
     else if(node == node -> parent -> left)
     node -> parent -> left = y;
     else
@@ -55,10 +71,12 @@ void INSERT_FIXUP(struct Node* node)
     {
         if(node -> parent == node -> parent -> parent -> left)
         {
+            // std::cout << "idharhai" << std::endl;
             struct Node* y;
             y = node -> parent -> parent -> right;
-            if(y -> color == "red")
+            if(y!=nil && y -> color == "red")
             {
+                // std::cout << "idharbhi" << std::endl;
                 // This is case 1
                 node -> parent -> color = "black";
                 y -> color = "black";
@@ -67,13 +85,16 @@ void INSERT_FIXUP(struct Node* node)
             }
             else if(node == node -> parent -> right)
             {
+                // std::cout << "auridharhai" << std::endl;
                 // This is case 2
                 node = node -> parent;
                 LEFT_ROTATE(node);
             }
+            // std::cout << "yahabhi" << std::endl;
             // This is case 3
             node -> parent -> color = "black";
             node -> parent -> parent -> color = "red";
+            // std::cout << node -> parent -> parent -> data << std::endl;
             RIGHT_ROTATE(node -> parent -> parent);
         }
         else
@@ -102,8 +123,9 @@ void INSERT_FIXUP(struct Node* node)
 }
 
 
-void INSERT(struct Node* node)
+void INSERT(int item)
 {
+    struct Node* node = newNode(item);
     struct Node* y;
     struct Node* x;
     y = nil;
@@ -123,31 +145,57 @@ void INSERT(struct Node* node)
     y -> left = node;
     else
     y -> right = node;
-    node -> left = nil;
-    node -> right = nil;
-    node -> color = "red";
-    // INSERT_FIXUP(node);
+    if(root!=node)
+    INSERT_FIXUP(node);
+    else
+    {
+        root -> color = "black";
+        // root -> parent = nil;
+    }
+}
+
+void preorder_traversal(struct Node* node)
+{
+    if(node!=nil)
+    {
+        std::cout << node -> data << " " << node -> color << std::endl;
+        preorder_traversal(node -> left);
+        preorder_traversal(node -> right);
+    }
 }
 
 int main()
 {
-    int nodes;
-    std::cin >> nodes;
-    // nil -> data = 0;
-    // nil -> left = NULL;
-    // nil -> right = NULL;
-    // nil -> color = "black";
-    root = nil;
-    // std::cout << "hello";
-    while(nodes--)
-    {
-        struct Node* node;
-        int x;
-        std::cin >> x;
-        // std::cout << "hello";
-        node -> data = x;
-        
-        INSERT(node);
-    }
+    // int nodes;
+    // std::cin >> nodes;
+    nil = (struct Node *) malloc(sizeof(struct Node));
+    nil -> color = "black";
+    nil -> left = NULL;
+    nil -> right = NULL;
+    nil -> parent = NULL;
+    root = nil; 
+    // while(nodes--)
+    // {
+    //     struct Node* node;
+    //     int x;
+    //     std::cin >> x;
+    //     INSERT(x);
+    // }
+    INSERT(11);
+    INSERT(2);
+    INSERT(1);
+    INSERT(7);
+    // std::cout << root -> color << std::endl;
+    preorder_traversal(root);
     return 0;
 }
+
+// 8
+// 11
+// 2
+// 1
+// 7
+// 5
+// 8
+// 14
+// 15
