d87f6043222d2e2155fe6c4d590be076927535c2
dB2510 <dhruvbodani2510@gmail.com>
2020-03-26 02:46:46 +0530

    starting RB-Tree

---
+++ Red_Black_Tree.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+struct Node{
+    struct Node* left;
+    struct Node* right;
+    int data;
+    std::string color;
+};
+
+int main()
+{
+    int nodes;
+    std::cin >> nodes;
+
+}
