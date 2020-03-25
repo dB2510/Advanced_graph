dd6ae21ea0abefa01bc050434d9827982e67ad0e
dB2510 <dhruvbodani2510@gmail.com>
2020-03-26 03:19:46 +0530

    left_rotate_done

--- Red_Black_Tree.cpp
+++ Red_Black_Tree.cpp
@@ -3,9 +3,34 @@
 struct Node{
     struct Node* left;
     struct Node* right;
+    struct Node* parent;
     int data;
     std::string color;
 };
+
+struct Node* nil;
+struct Node* root;
+
+void LEFT_ROTATE(struct Node* node)
+{
+    struct Node* y;
+    y = node -> right;
+    node -> right = y -> left;
+    if(y -> left != nil)
+    y -> left -> parent = node;
+    y -> parent = node -> parent;
+    if(node -> parent != nil)
+    root = y;
+    else if(node == node -> parent -> left)
+    node -> parent -> left = y;
+    else
+    node -> parent -> right = y;
+
+    y -> left = node;
+    node -> parent = y;
+}
+
+void RIGHT_ROTATE(struct N)
 
 int main()
 {
